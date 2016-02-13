e412936ecd50f491598f8f9fd4bbc8b28a9bb455
Rohan Barman <Rohan1Barman@gmail.com>
2016-02-12 22:00:28 -0500

    v1 Simple Wireframe * material-ui to get textfield,button, and list * more components will be introduced later

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,12 +1,37 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var RaisedButton = require('material-ui/lib/raised-button');
+var List = require('material-ui/lib/lists/list');
+var ListItem = require('material-ui/lib/lists/list-item');
+var AppBar =  require('material-ui/lib/app-bar');
+var TextField = require('material-ui/lib/text-field');
 
 // creating the Main component
 var Main = React.createClass({
 	render: function() {
 		return (
+
 			<div> 
-				Starting Sound Share!
+
+				<AppBar
+					title="SoundShare"
+					showMenuIconButton={false}
+				/>
+				<List>
+					Queue
+				      <ListItem primaryText="Song 1"/>
+				      <ListItem primaryText="Song 2"/>
+				      <ListItem primaryText="Song 3"/>
+				</List>
+				<TextField 
+					hintText="Song URL"
+				/>
+				<br/>
+				<RaisedButton
+					label="Add to Queue"
+					secondary={true}
+				/>
+
 			</div>
 		)
 	}
